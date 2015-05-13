d0a674567018ce055f5240d3050bb0db5d9879c1
Wen Bo Xie <wenbox323@gmail.com>
2015-05-13 19:05:52 -0400

    Move skip_before_action to the top

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  
+  skip_before_action :flash_attack, only: [ :index, :new]
+
   def index
     @posts = Post.all
   end
@@ -36,6 +39,4 @@ class PostsController < ApplicationController
       render :edit
     end
   end
-
-  skip_before_action :flash_attack, only: [ :index, :new]
 end
