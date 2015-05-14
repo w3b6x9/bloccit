6532c050e4494b94c9fbab6e5fa1a68866953a1e
Wen Bo Xie <wenbox323@gmail.com>
2015-05-13 21:51:51 -0400

    32-associations-assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { order('title ASC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
 end
