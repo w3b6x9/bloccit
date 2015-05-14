d212d9459848978ca5dd7486f6d656e031462849
Wen Bo Xie <wenbox323@gmail.com>
2015-05-13 20:57:06 -0400

    32-association-checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426192330) do
+ActiveRecord::Schema.define(version: 20150513231352) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -36,7 +36,10 @@ ActiveRecord::Schema.define(version: 20150426192330) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
