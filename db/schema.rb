e6259e2b8611c7242879a1c347e3d885ca6ff156
Wen Bo Xie <wenbox323@gmail.com>
2015-05-21 10:38:27 -0400

    38-uploading-images-checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516231623) do
+ActiveRecord::Schema.define(version: 20150520034803) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20150516231623) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
