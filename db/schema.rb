4b64c5f49eb9996a64473472bca1f83691a1bb15
Wen Bo Xie <wenbox323@gmail.com>
2015-05-24 17:58:48 -0400

    38-uploading-images-assignment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520034803) do
+ActiveRecord::Schema.define(version: 20150524212148) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150520034803) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -50,6 +51,15 @@ ActiveRecord::Schema.define(version: 20150520034803) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "summaries", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "summaries", ["post_id"], name: "index_summaries_on_post_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
