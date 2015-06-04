5d44d5fabb1dbe06a8cc4ed5efeb8c4c897f5c50
Wen Bo Xie <wenbox323@gmail.com>
2015-06-03 21:29:23 -0400

    40-comments-checkpoint

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,15 @@ class CommentsController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
-    @comment = post.comments.build(comment_params.merge(user_id: current_user.id))
-
+    @comment = @post.comments.build(comment_params.merge(user_id: current_user.id))
     authorize @comment
 
     if @comment.save
-      redirect_to [@topic, @post], notice: "Comment successfully created!"
+      flash[:notice] = "Comment successfully created!"
+      redirect_to [@topic, @post]
     else
-      redirect_to [@topic, @post], error: "Comment failed to be created!"
+      flash[:error] = "Comment failed to be created. Try again!"
+      redirect_to [@topic, @post]
     end
   end
 
