623f9a6472c761b970ec9e99d148069d140bec7e
Wen Bo Xie <wenbox323@gmail.com>
2015-06-04 23:24:22 -0400

    WIP

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,29 @@
 class CommentsController < ApplicationController
   def create
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params.merge(user_id: current_user.id))
     authorize @comment
 
     if @comment.save
       flash[:notice] = "Comment successfully created!"
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment failed to be created. Try again!"
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    authorize @comment
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "Comment couldn't be deleted. Try again."
+      redirect_to [@post.topic, @post]
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
   resources :users, only: [:update]
 
   resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only: [:create, :destroy]
-    end
+    resources :posts, except: [:index]
   end
 
-  post '/up-vote' => 'votes#up_vote', as: :up_vote
-  post '/down-vote' => 'votes#down_vote', as :down_vote
-  
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as :down_vote
+  end
+
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
