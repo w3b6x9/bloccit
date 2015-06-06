17a36c885411ceb5e9af641ea5c70021adf81766
Wen Bo Xie <wenbox323@gmail.com>
2015-06-06 18:38:22 -0400

    50-mocking-with-rspec-assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -37,4 +37,11 @@ class Post < ActiveRecord::Base
   def create_vote
     user.votes.create(value: 1, post: self)
   end
+
+  def save_with_initial_vote
+    ActiveRecord::Base.transaction do 
+      save
+      user.votes.create(value: 1, post: self)
+    end
+  end
 end
