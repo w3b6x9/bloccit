a3e47a68b21a0b7fc9be7180f67ec8493362fce3
Wen Bo Xie <wenbox323@gmail.com>
2015-06-06 18:09:08 -0400

    46-comments implement new? method in CommentPolicy

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,4 +1,8 @@
 class CommentPolicy < ApplicationPolicy
+  def new?
+    user.present?
+  end
+  
   def destroy?
     user.present? && (record.user == user || user.admin? || user.moderator?)
   end
