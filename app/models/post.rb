d33ab4059faaf324dfe647837536b849a59163bb
Wen Bo Xie <wenbox323@gmail.com>
2015-06-06 00:01:10 -0400

    49-voting-assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -25,10 +25,18 @@ class Post < ActiveRecord::Base
     update_attribute(:rank, new_rank)
   end
 
+  after_create :create_vote
+
   default_scope { order('rank DESC') }
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
   # validates :topic, presence: true
   # validates :user, presence: true
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
 end
