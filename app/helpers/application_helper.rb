fe3d817e1d72c6a984b7222b8534cafdb27db482
Wen Bo Xie <wenbox323@gmail.com>
2015-06-07 17:07:09 -0400

    53-another-interlude-assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,20 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end
+
+  def up_vote_link_classes(post)
+    if current_user.voted(post) && current_user.voted(post).up_vote?
+      'voted'
+    else
+      ''
+    end
+  end
+
+  def down_vote_link_classes(post)
+    if current_user.voted(post) && current_user.voted(post).down_vote?
+      'voted'
+    else
+      ''
+    end
+  end
 end
