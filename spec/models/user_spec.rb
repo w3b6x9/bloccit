1a805ff5cc24d6613f002bf565319a6341771c92
Wen Bo Xie <wenbox323@gmail.com>
2015-06-06 23:39:39 -0400

    51-favoriting-assignment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,25 @@ describe User do
 
   include TestFactories
 
-  describe "#favorited(post)" do 
-    xit "returns 'nil' if the user has not favorited the post" do 
+  describe "#favorited(post)" do
+
+    before do 
+      @post = associated_post
+      @user = authenticated_user
     end
 
-    xit "returns the appropriate favorite if it exists" do 
+    it "returns 'nil' if the user has not favorited the post" do 
+      expect( @user.favorites.find_by_post_id(@post.id) ).to be_nil
+    end
+
+    it "returns the appropriate favorite if it exists" do 
+      favorite = @user.favorites.where(post: @post).create
+      expect( @user.favorites.find_by_post_id(@post.id) ).not_to be_nil
+    end
+
+    it "returns 'nil' if the user has favorited another post" do 
+      favorite = @user.favorites.where(post: @post).create
+      expect( @user.favorites.find_by_post_id(@post.id+1) ).to be_nil
     end
   end
 end
