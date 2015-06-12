166806f9431287f581f4fca5d7a94091065bd853
Wen Bo Xie <wenbox323@gmail.com>
2015-06-12 18:23:01 -0400

    57-destroying-with-ajax-checkpoint

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,29 +16,17 @@ class CommentsController < ApplicationController
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-
     authorize @comment
+
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@post.topic, @post]
     end
-  end
 
-  def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
-
-    authorize @comment
-    if @comment.destroy
-      flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
-    else
-      flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
