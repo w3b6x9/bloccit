a3a7a8a6527b80370ea09630ce5f7f972c677548
Wen Bo Xie <wenbox323@gmail.com>
2015-06-12 15:31:33 -0400

    56-testing-users-assignment

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,19 @@ FactoryGirl.define do
     password "helloworld"
     password_confirmation "helloworld"
     confirmed_at Time.now
+
+    factory :user_with_post_and_comment do
+      after(:create) do |user|
+        post = create(:post, user: user)
+        comment = create(:comment, user: user, post: post)
+      end
+    end
+
+    factory :user_with_post_and_two_comments do
+      after(:create) do |user|
+        post = create(:post, user: user)
+        2.times { create(:comment, user: user, post: post) }
+      end
+    end
   end
 end
